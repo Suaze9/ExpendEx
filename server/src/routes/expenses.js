5ae6f23295b2b9b4b1bc794a59da2d944810052c
suazodaniel99 <suazodaniel99@gmail.com>
2020-07-12 12:15:26 -0600

    Basic cost sum

--- server/src/routes/expenses.js
+++ server/src/routes/expenses.js
@@ -26,6 +26,63 @@ router.get('/', validate, async (req, res) => {
 
 })
 
+router.post('/date/', validate, async (req, res) => {
+    const body = req.body;
+
+    if(!body.filter){
+        res.status(400).send('No filter provided');
+        return;
+    }
+
+    let query = { user: req.auth._id };
+
+    switch(body.filter){
+        case 'month':{
+            const now = new Date();
+            const startDate = new Date(now.getFullYear(), now.getMonth(), 1);
+            const endDate = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+            query.date = {"$gte": startDate, "$lt": endDate};
+            break;
+        }
+        case 'range':{
+            if(!body.range){
+                res.status(400).send('No filter provided');
+                return;
+            }
+            try{
+                const startDate = new Date(body.range.start);
+                const endDate = new Date(body.range.end);
+                endDate.setHours(23,59,59,999);
+                query.date = {"$gte": startDate, "$lt": endDate};
+            }catch(err){
+                res.status(400).send('Bad filter provided');
+                return;
+            }
+            break;
+        }
+        default:{
+            res.status(400).send('Invalid filter provided');
+            return;
+        }
+    }
+
+    let exps = await Expenses.find(query);
+    if(!exps){
+        res.status(500).send('Internal Error...');
+        return;
+    }
+
+    const expArr = [];
+
+    exps.forEach((exp)=>{
+        expArr.push({ id: exp._id, cost: exp.cost, type: exp.type});
+    })
+
+    res.status(200).send({expenses: expArr});
+    
+
+})
+
 router.get('/p/:id', validate, async (req, res) => {
     const findId = req.params.id;
 
--- server/src/routes/stats.js
+++ server/src/routes/stats.js
@@ -58,13 +58,145 @@ router.post('/total/', validate, async (req, res) => {
     }
 
     const expArr = [];
+    const total = exps.reduce((sum, exp)=> sum + exp.cost);
 
-    exps.forEach((exp)=>{
-        expArr.push({ id: exp._id, cost: exp.cost, type: exp.type});
-    })
+    res.status(200).send({ filter: body.filter, total });
 
-    res.status(200).send({expenses: expArr});
+})
+
+router.post('/total/category/:id', validate, async (req, res) => {
+    const body = req.body;
+    const findId = req.params.id;
+
+    if(!isValidObjectId(findId)){
+        res.status(400).send('Invalid Category id');
+        return;
+    }
     
+    const cat = await Categories.findOne({ _id: findId, user: req.auth._id});
+    if(!cat){
+        res.status(404).send('Category not found');
+        return;
+    }
+
+    if(!body.filter){
+        res.status(400).send('No filter provided');
+        return;
+    }
+
+    let query = { user: req.auth._id, category: findId };
+
+    switch(body.filter){
+        case 'month':{
+            const now = new Date();
+            const startDate = new Date(now.getFullYear(), now.getMonth(), 1);
+            const endDate = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+            query.date = {"$gte": startDate, "$lt": endDate};
+            break;
+        }
+        case 'range':{
+            if(!body.range){
+                res.status(400).send('No filter provided');
+                return;
+            }
+            try{
+                const startDate = new Date(body.range.start);
+                const endDate = new Date(body.range.end);
+                endDate.setHours(23,59,59,999);
+                query.date = {"$gte": startDate, "$lt": endDate};
+            }catch(err){
+                res.status(400).send('Bad filter provided');
+                return;
+            }
+            break;
+        }
+        case 'all':{
+            break;
+        }
+        default:{
+            res.status(400).send('Invalid filter provided');
+            return;
+        }
+    }
+
+    let exps = await Expenses.find(query);
+    if(!exps){
+        res.status(500).send('Internal Error...');
+        return;
+    }
+
+    const expArr = [];
+    const total = exps.reduce((sum, exp)=> sum + exp.cost);
+
+    res.status(200).send({ filter: body.filter, total });
+
+})
+
+router.post('/total/expenseType/:id', validate, async (req, res) => {
+    const body = req.body;
+    const findId = req.params.id;
+
+    if(!isValidObjectId(findId)){
+        res.status(400).send('Invalid ExpenseType id');
+        return;
+    }
+    
+    const exp = await ExpenseTypes.findOne({ _id: findId, user: req.auth._id});
+    if(!exp){
+        res.status(404).send('ExpenseType not found');
+        return;
+    }
+
+    if(!body.filter){
+        res.status(400).send('No filter provided');
+        return;
+    }
+
+    let query = { user: req.auth._id, category: findId };
+
+    switch(body.filter){
+        case 'month':{
+            const now = new Date();
+            const startDate = new Date(now.getFullYear(), now.getMonth(), 1);
+            const endDate = new Date(now.getFullYear(), now.getMonth() + 1, 1);
+            query.date = {"$gte": startDate, "$lt": endDate};
+            break;
+        }
+        case 'range':{
+            if(!body.range){
+                res.status(400).send('No filter provided');
+                return;
+            }
+            try{
+                const startDate = new Date(body.range.start);
+                const endDate = new Date(body.range.end);
+                endDate.setHours(23,59,59,999);
+                query.date = {"$gte": startDate, "$lt": endDate};
+            }catch(err){
+                res.status(400).send('Bad filter provided');
+                return;
+            }
+            break;
+        }
+        case 'all':{
+            break;
+        }
+        default:{
+            res.status(400).send('Invalid filter provided');
+            return;
+        }
+    }
+
+    let exps = await Expenses.find(query);
+    if(!exps){
+        res.status(500).send('Internal Error...');
+        return;
+    }
+
+    const expArr = [];
+    const total = exps.reduce((sum, exp)=> sum + exp.cost);
+
+    res.status(200).send({ filter: body.filter, total });
 
 })
 
